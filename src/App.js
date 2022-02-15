dfb940915cf23b7d3bcec7e10cbeb49937786bb9
Bartek-str <bartlomiejstruzyk@gmail.com>
2022-02-15 13:00:36 +0100

    timer unit tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+export const useCountDown = (seconds = 30) => {
+  const [timeStarted, setTimeStarted] = useState(new Date().getTime())
+  const [timeLeft, setTimeLeft] = useState(seconds)
+  const [isFinished, setIsFinished] = useState(false)
+
+  useEffect(() => {
+    setTimeStarted(new Date().getTime())
+
+    const timeout = setTimeout(() => setIsFinished(true), [seconds * 1000])
+
+    const interval = setInterval(() => {
+      setTimeLeft(Math.ceil(((seconds * 1000) - (new Date().getTime() - timeStarted)) / 1000))
+      if (timeLeft === 0 || timeLeft < 0) {
+        clearInterval(interval)
+      }
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeout)
+      clearInterval(interval)
+    }
+  }, [])
+
+  return {
+    timeLeft,
+    isFinished
+  }
+}
 
 function App() {
+  const { timeLeft, isFinished } = useCountDown(15)
+
   return (
     <div className="App">
       <header className="App-header">
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useCountDown } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('useCountDown', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'setTimeout')
+  })
+
+  afterEach(() => {
+    jest.useFakeTimers()
+  })
+
+  it('hook renders properly', () => {
+    const { result } = renderHook(() => useCountDown(15))
+
+    // one call is Function: _flushCallback with 0
+    expect(setTimeout).toHaveBeenCalledTimes(2)
+    expect(result.current.timeLeft).toBe(15)
+    expect(result.current.isFinished).toBe(false)
+  })
+
+  it('timeout', () => {
+    const { result } = renderHook(() => useCountDown(15))
+
+    act(() => {
+      jest.advanceTimersByTime(15 * 1000)
+    })
+
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+    expect(result.current.timeLeft).toBe(0)
+    expect(result.current.isFinished).toBe(true)
+  })
+
+  it('1 second', () => {
+    const { result } = renderHook(() => useCountDown(15))
+
+    act(() => {
+      jest.advanceTimersByTime(1000)
+    })
+
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+    expect(result.current.timeLeft).toBe(14)
+    expect(result.current.isFinished).toBe(false)
+  })
+
+  it('10 seconds', () => {
+    const { result } = renderHook(() => useCountDown(15))
+
+    act(() => {
+      jest.advanceTimersByTime(10 * 1000)
+    })
+
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+    expect(result.current.timeLeft).toBe(5)
+    expect(result.current.isFinished).toBe(false)
+  })
+})
